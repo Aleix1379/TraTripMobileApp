3cc27fa781eae447c981423c42d21b7c6cfb7340
Aleix <aleixmp1379@gmail.com>
2022-06-30 21:47:18 +0200

    Add animation

--- src/components/BottomNavigation/NavigationButton.tsx
+++ src/components/BottomNavigation/NavigationButton.tsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Animated } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import useTheme from '../../styles/useTheme'
@@ -20,6 +20,8 @@ interface NavButtonProps {
   isActive: boolean
 }
 
+const ANIMATION_DURATION = 1000
+
 const NavButton: React.FC<NavButtonProps> = ({ label, to, icon, isActive }) => {
   const navigation = useNavigation<homeScreenProp>()
   const theme = useTheme()
@@ -32,22 +34,52 @@ const NavButton: React.FC<NavButtonProps> = ({ label, to, icon, isActive }) => {
     heart: faHeart
   }
 
+  const [upperAnimation] = useState(new Animated.Value(0))
+
+  const startAnimation = () => {
+    Animated.timing(upperAnimation, {
+      useNativeDriver: true,
+      toValue: isActive ? 1 : 0,
+      duration: 350
+    }).start()
+  }
+
+  const animatedStyles = {
+    upper: {
+      transform: [
+        {
+          translateX: upperAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [-50, 0]
+          })
+        }
+      ]
+    }
+  }
+
+  useEffect(() => {
+    startAnimation()
+  }, [isActive])
+
   return (
     <View style={[styles.bar, { backgroundColor: colors.BACKGROUND }]}>
-      <View
-        onTouchEnd={() => navigation.navigate(to)}
-        style={[
-          styles.item,
-          {
-            backgroundColor: isActive ? colors.ACCENT : 'transparent'
-          }
-        ]}>
+      <View onTouchEnd={() => navigation.navigate(to)} style={[styles.item]}>
         <FontAwesomeIcon
           icon={icons[icon]}
           size={22}
-          style={{ color: colors.TEXT }}
+          style={{ color: colors.TEXT, zIndex: 1 }}
         />
-        {isActive && <Text style={styles.label}>{label}</Text>}
+
+        <Animated.View
+          style={[
+            styles.background,
+            {
+              backgroundColor: isActive ? colors.ACCENT : 'transparent'
+            },
+            animatedStyles.upper
+          ]}
+        />
+        <Text style={[styles.label]}>{label}</Text>
       </View>
     </View>
   )
@@ -65,13 +97,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     padding: 8,
-    borderRadius: 15
+    borderRadius: 15,
+    overflow: 'hidden'
   },
   button: {},
   label: {
     fontWeight: 'bold',
     marginLeft: 5,
     fontSize: 12
+  },
+  background: {
+    height: 38,
+    width: '128%',
+    borderRadius: 15,
+    padding: 8,
+    position: 'absolute',
+    top: 0,
+    left: 0
   }
 })
 
