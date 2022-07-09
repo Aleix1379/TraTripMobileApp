703d6f2a2161835320244908b7951f844e8f1c7b
Aleix <aleixmp1379@gmail.com>
2022-07-09 15:59:08 +0200

    Add Score test

---
+++ __tests__/Score-test.tsx
@@ -0,0 +1,14 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Score from '../src/components/Score'
+
+jest.mock('@fortawesome/react-native-fontawesome', () => ({
+  FontAwesomeIcon: () => ''
+}))
+
+// tests render correctly mocking FontAwesomeIcon
+it('renders correctly', () => {
+  const tree = renderer.create(<Score score={3} />)
+  expect(tree).toMatchSnapshot()
+})
